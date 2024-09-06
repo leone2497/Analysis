44ac856e1632d1cb4fb7ea18da3bf9eb228e15fe
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2024-09-06 22:52:53 +0200

    Update Car market australia project.py

--- Car market australia project.py
+++ Car market australia project.py
@@ -4,7 +4,7 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 
 # Load dataset
-cars_market_australia = pd.read_csv("car market australia.csv")
+cars_market_australia = pd.read_csv("https://github.com/leone2497/Analysis/blob/main/car%20market%20australia.csv")
 st.title("Australia Cars Market Analysis")
 
 # Sidebar selection
