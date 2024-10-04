f9fafae460c957a4fbfd636049a58715b56aae60
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2024-10-04 13:26:03 +0200

    Update Energy analysis.py

--- Energy analysis.py
+++ Energy analysis.py
@@ -5,3 +5,8 @@ import streamlit as st
 
 Data=pd.read_csv('https://github.com/leone2497/Analysis/blob/main/owid-energy-data.csv')
 
+
+st.title("Energy market - dashboard")
+st.sidebar.title("type of analysis")
+dictionary_type = st.sidebar.selectbox("Analysis", ["Dataset"])
+
